cf7910c6a55ce09b7647ce6f69b6721e15d67aa9
Frank Fang <FrankFang1990@gmail.com>
2018-06-21 11:29:50 +0800

    使用 parcel 零配置搭建

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Button from './button'
+
+
+new Vue({
+  el: '#app'
+})
--- src/button.js
+++ src/button.js
@@ -1,3 +1,4 @@
+import Vue from 'vue'
 Vue.component('g-button', {
   template: `
     <button class="g-button"><slot></slot></button>
