e7074e0db9e3d3d05f657ad09d1abb921dffbe65
Frank Fang <FrankFang1990@gmail.com>
2018-06-22 13:38:57 +0800

    添加 icon.vue

---
+++ src/icon.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg class="g-icon">
+    <use :xlink:href="`#i-${name}`"></use>
+  </svg>
+</template>
+<script>
+  export default {
+    name: 'icon',
+    props: ['name']
+  }
+</script>
+<style lang="scss">
+  .g-icon {
+    width: 1em;height: 1em; vertical-align: middle;
+  }
+</style>
