13dd499d49a1f91ae56a20867f6f18f70386da38
Frank Fang <FrankFang1990@gmail.com>
2018-06-23 15:50:11 +0800

    整理代码

--- src/button.vue
+++ src/button.vue
@@ -15,31 +15,15 @@
 </script>
 <style lang="scss">
   @keyframes spin {
-    0% {
-      transform: rotate(0deg);
-    }
-    100% {
-      transform: rotate(360deg);
-    }
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
   }
   .g-button {
-    height: var(--button-height);
-    padding: 0 1em;
-    font: inherit;
-    border-radius: var(--border-radius);
-    border: 1px solid var(--border-color);
-    background: var(--button-bg);
-    &:hover {
-      border-color: var(--border-color-hover);
-    }
-    &:active {
-      background: var(--button-active-bg);
-    }
-    &:focus {
-      outline: none;
-    }
-    .spin {
-      animation: spin 1s infinite linear;
-    }
+    height: var(--button-height); padding: 0 1em; font: inherit; border-radius: var(--border-radius);
+    border: 1px solid var(--border-color); background: var(--button-bg);
+    &:hover { border-color: var(--border-color-hover); }
+    &:active { background: var(--button-active-bg); }
+    &:focus { outline: none; }
+    .spin { animation: spin 1s infinite linear; }
   }
 </style>
