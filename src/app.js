efb7d3c4cd39c4c245fef4eeb734ff8f87c3c896
Frank Fang <FrankFang1990@gmail.com>
2018-06-24 15:20:49 +0800

    使用 chai.expect 测试

--- src/app.js
+++ src/app.js
@@ -2,13 +2,71 @@ import Vue from 'vue'
 import Button from './button'
 import ButtonGroup from './button-group'
 
-Vue.component('g-button', Button)
-Vue.component('g-button-group', ButtonGroup)
+import chai from 'chai'
+const expect = chai.expect
 
-new Vue({
-  el: '#app',
-  data: {
-    loading1: false,
-    loading2: true,
-  }
-})
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  }).$mount()
+  const useElement = vm.$el.querySelector('use')
+  expect(useElement.getAttribute('xlink:href')).to.equal('#i-settings')
+  vm.$destroy()
+}
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  }).$mount()
+  const useElements = vm.$el.querySelectorAll('use')
+  expect(useElements.length).to.equal(1)
+  expect(useElements[0].getAttribute('xlink:href')).to.equal('#i-loading')
+  vm.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+    }
+  }).$mount(div)
+  const icon = vm.$el.querySelector('svg')
+  expect(getComputedStyle(icon).order).to.eq('1')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  }).$mount(div)
+  const icon = vm.$el.querySelector('svg')
+  expect(getComputedStyle(icon).order).to.eq('2')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+    }
+  }).$mount()
+  vm.$on('click', function(){
+    console.log('hi')
+  })
+  vm.$el.click()
+}
