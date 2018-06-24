c0107446a57d213d3a98a48b24ed1074f70f67c1
Frank Fang <FrankFang1990@gmail.com>
2018-06-24 21:50:50 +0800

    第一个元素不需要 margin left -1px

--- src/button-group.vue
+++ src/button-group.vue
@@ -21,7 +21,9 @@
     vertical-align: middle;
     > .g-button {
       border-radius: 0;
-      margin-left: -1px;
+      &:not(:first-child) {
+        margin-left: -1px;
+      }
       &:first-child {
         border-top-left-radius: var(--border-radius);
         border-bottom-left-radius: var(--border-radius);
