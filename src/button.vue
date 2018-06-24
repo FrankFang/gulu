070cff488c91418fdcd2065f6bec20b3604307d1
Frank Fang <FrankFang1990@gmail.com>
2018-06-24 21:06:54 +0800

    闭合标签

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,8 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-    <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+    <g-icon class="icon" v-if="icon" :name="icon"/>
     <div class="content">
-      <slot></slot>
+      <slot/>
     </div>
   </button>
 </template>
