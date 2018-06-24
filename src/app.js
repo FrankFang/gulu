d8dbba346f7dc587bcc07e3eed38e16107b78982
Frank Fang <FrankFang1990@gmail.com>
2018-06-24 15:59:30 +0800

    使用 puppeteer 自动测试

--- src/app.js
+++ src/app.js
@@ -8,69 +8,76 @@ import spies from 'chai-spies'
 chai.use(spies)
 const expect = chai.expect
 
-{
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings'
-    }
-  }).$mount()
-  const useElement = vm.$el.querySelector('use')
-  expect(useElement.getAttribute('xlink:href')).to.equal('#i-settings')
-  vm.$destroy()
-}
-{
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings',
-      loading: true
-    }
-  }).$mount()
-  const useElements = vm.$el.querySelectorAll('use')
-  expect(useElements.length).to.equal(1)
-  expect(useElements[0].getAttribute('xlink:href')).to.equal('#i-loading')
-  vm.$destroy()
-}
-{
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings',
-    }
-  }).$mount(div)
-  const icon = vm.$el.querySelector('svg')
-  expect(getComputedStyle(icon).order).to.eq('1')
-  vm.$el.remove()
-  vm.$destroy()
-}
-{
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings',
-      iconPosition: 'right'
-    }
-  }).$mount(div)
-  const icon = vm.$el.querySelector('svg')
-  expect(getComputedStyle(icon).order).to.eq('2')
-  vm.$el.remove()
-  vm.$destroy()
-}
-{
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings',
-    }
-  }).$mount()
-  const spy = chai.spy(() => {})
-  vm.$on('click', spy)
-  vm.$el.click()
-  expect(spy).to.have.been.called()
+try {
 
+  {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings'
+      }
+    }).$mount()
+    const useElement = vm.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to.equal('#i-settings2')
+    vm.$destroy()
+  }
+  {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+        loading: true
+      }
+    }).$mount()
+    const useElements = vm.$el.querySelectorAll('use')
+    expect(useElements.length).to.equal(1)
+    expect(useElements[0].getAttribute('xlink:href')).to.equal('#i-loading')
+    vm.$destroy()
+  }
+  {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+      }
+    }).$mount(div)
+    const icon = vm.$el.querySelector('svg')
+    expect(getComputedStyle(icon).order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+  }
+  {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+        iconPosition: 'right'
+      }
+    }).$mount(div)
+    const icon = vm.$el.querySelector('svg')
+    expect(getComputedStyle(icon).order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+  }
+  {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+      }
+    }).$mount()
+    const spy = chai.spy(() => {})
+    vm.$on('click', spy)
+    vm.$el.click()
+    expect(spy).to.have.been.called()
+
+  }
+} catch (error) {
+  window.errors = [error]
+} finally {
+  console.log(window.errors)
 }
---
+++ test.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({headless: true})
+  const page = await browser.newPage()
+  await page.setRequestInterception(true);
+  page.on('request', request => {
+    if (request.resourceType() === 'image')
+      request.abort();
+    else
+      request.continue();
+  })
+  page.once('load', async () => {
+    const errors = await page.evaluate(() => {
+      return window.errors || []
+    })
+    if (errors.length === 0) {
+      console.info('没有错误')
+    } else {
+      console.error('有错误')
+      errors.forEach((error) => {
+        console.log(error.message)
+      })
+    }
+    await browser.close()
+  });
+  await page.goto('http://127.0.0.1:1234')
+})()
