4ebf22e79147334d9e7209d820e0f08262c61721
Frank Fang <FrankFang1990@gmail.com>
2018-06-24 21:05:54 +0800

    更新 button 里面 icon 的样式

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,6 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-    <g-icon v-if="icon" :name="icon"></g-icon>
+    <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
     <div class="content">
       <slot></slot>
     </div>
