7d3185b7a6d430d8ce5e8dc5d4329532dbd30a7d
Frank Fang <FrankFang1990@gmail.com>
2018-06-24 18:29:35 +0800

    让测试的输出样式更好看

--- karma.conf.js
+++ karma.conf.js
@@ -36,7 +36,7 @@ module.exports = function (config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['mocha'],
 
 
     // web server port
@@ -59,7 +59,6 @@ module.exports = function (config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
-
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
