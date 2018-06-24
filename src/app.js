dd5c9e081a12514c901180346d9726ae93a20cb3
Frank Fang <FrankFang1990@gmail.com>
2018-06-24 18:46:24 +0800

    app.js 没用了

--- src/app.js
+++ src/app.js
@@ -2,37 +2,4 @@ import Vue from 'vue'
 import Button from './button'
 import ButtonGroup from './button-group'
 
-import chai from 'chai'
-import spies from 'chai-spies'
 
-chai.use(spies)
-const expect = chai.expect
-
-try {
-
-  {
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-      propsData: {
-        icon: 'settings'
-      }
-    }).$mount()
-    const useElement = vm.$el.querySelector('use')
-    expect(useElement.getAttribute('xlink:href')).to.equal('#i-settings2')
-    vm.$destroy()
-  }
-  {
-  }
-  {
-  }
-  {
-  }
-  {
-  }
-} catch (error) {
-  window.errors = [error]
-} finally {
-  window.errors.forEach((error) => {
-    console.error(error.message)
-  })
-}
