4f504555d8a1df8cc7c94ccd0e547e6d958a41e4
Frank Fang <FrankFang1990@gmail.com>
2018-06-24 21:12:26 +0800

    添加菊花状态

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
     <g-icon class="icon" v-if="icon" :name="icon"/>
+    <g-icon class="loading" name="loading"></g-icon>
     <div class="content">
       <slot/>
     </div>
@@ -22,6 +23,10 @@
   }
 </script>
 <style lang="scss">
+  @keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+  }
   .g-button { font-size: var(--font-size); height: var(--button-height); padding: 0 1em;
     border-radius: var(--border-radius); border: 1px solid var(--border-color);
     background: var(--button-bg);
@@ -36,6 +41,9 @@
     &.icon-right {
       > .content { order: 1; }
       > .icon { order: 2; margin-right: 0; margin-left: .1em;}
+    }
+    .loading{
+      animation: spin 2s infinite linear;
     }
   }
 </style>
