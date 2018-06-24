3d6ac2c913bd6ba5cb8192742df9b5df7b8659a3
Frank Fang <FrankFang1990@gmail.com>
2018-06-24 18:15:43 +0800

    使用 Karma & Chai 测试

---
+++ test/button.test.js
@@ -0,0 +1,21 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Button from '../src/button'
+
+describe('Button', () => {
+  it('可以显示', () => {
+    expect(Button).to.be.ok
+  })
+  it('可以设置icon.', (done) => {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings'
+      }
+    }).$mount()
+    const useElement = vm.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to.equal('#i-settings')
+    vm.$destroy()
+    done()
+  })
+})
