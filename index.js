e3bc17b58b0137d1dc9688d14c2dcb64d4df13ab
Frank Fang <FrankFang1990@gmail.com>
2018-06-30 16:05:53 +0800

    使用相对路径

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import Button from 'src/button'
-import ButtonGroup from 'src/button-group'
-import Icon from 'src/icon'
+import Button from './src/button'
+import ButtonGroup from './src/button-group'
+import Icon from './src/icon'
 
 export {Button, ButtonGroup, Icon}
