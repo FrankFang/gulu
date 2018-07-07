5483eba2754c64140556f6713930fbad438f856e
Frank Fang <FrankFang1990@gmail.com>
2018-07-07 21:36:24 +0800

    基本实现 row 和 col 的功能

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+    :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
+    <div style="border: 1px solid green; height: 100px;">
+      <slot></slot>
+    </div>
   </div>
 </template>
 <script>
@@ -9,20 +12,37 @@
     props: {
       span: {
         type: [Number, String]
+      },
+      offset: {
+        type: [Number, String]
+      },
+    },
+    data () {
+      return {
+        gutter: 0
       }
-    }
+    },
+    created () {
+      console.log('col created')
+    },
+    mounted () {
+      console.log('col mounted')
+    },
   }
 </script>
 <style scoped lang="scss">
   .col {
-    height: 100px;
-    background: grey;
     width: 50%;
-    border: 1px solid red;
     $class-prefix: col-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         width: ($n / 24) * 100%;
+      }
+    }
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
       }
     }
   }
--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,33 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px'}">
     <slot></slot>
   </div>
 </template>
+<script>
+  export default {
+    name: 'GuluRow',
+    props: {
+      gutter: {
+        type: [Number, String]
+      }
+    },
+    created () {
+      console.log('row created')
+    },
+    mounted () {
+      console.log('row mounted')
+      console.log(this.$children)
+      this.$children.forEach((vm) => {
+        vm.gutter = this.gutter
+      })
+    }
+  }
+
+  var div = document.createElement('div') // created
+  var childDiv = document.createElement('div') // child created
+  div.appendChild(childDiv) // child mounted
+  document.body.appendChild(div) // mounted
+</script>
 <style scoped lang="scss">
   .row{
     display: flex;
