98d0bd42f44b9296181d42908efb52142481b550
Frank Fang <FrankFang1990@gmail.com>
2018-07-07 22:57:31 +0800

    默认就是phone样式

--- src/col.vue
+++ src/col.vue
@@ -23,7 +23,6 @@
       offset: {
         type: [Number, String]
       },
-      phone: {type: Object, validator,},
       ipad: {type: Object, validator,},
       narrowPc: {type: Object, validator,},
       pc: {type: Object, validator,},
@@ -36,12 +35,10 @@
     },
     computed: {
       colClass () {
-        let {span, offset, phone, ipad, narrowPc, pc, widePc} = this
-        let phoneClass = []
+        let {span, offset, ipad, narrowPc, pc, widePc} = this
         return [
           span && `col-${span}`,
           offset && `offset-${offset}`,
-          ... (phone && [`col-phone-${phone.span}`]),
           ... (ipad && [`col-ipad-${ipad.span}`]),
           ... (narrowPc && [`col-narrow-pc-${narrowPc.span}`]),
           ... (pc && [`col-pc-${pc.span}`]),
@@ -69,20 +66,6 @@
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         margin-left: ($n / 24) * 100%;
-      }
-    }
-    @media (max-width: 576px) {
-      $class-prefix: col-phone-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          width: ($n / 24) * 100%;
-        }
-      }
-      $class-prefix: offset-phone-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          margin-left: ($n / 24) * 100%;
-        }
       }
     }
     @media (min-width: 577px) and (max-width: 768px) {
