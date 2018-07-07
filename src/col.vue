11d64f3a271a6c15004ba365327c78595c019278
Frank Fang <FrankFang1990@gmail.com>
2018-07-07 21:52:26 +0800

    重构 row 和 col

--- src/col.vue
+++ src/col.vue
@@ -1,9 +1,6 @@
 <template>
-  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-    :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
-    <div style="border: 1px solid green; height: 100px;">
-      <slot></slot>
-    </div>
+  <div class="col" :class="colClass" :style="colStyle">
+    <slot></slot>
   </div>
 </template>
 <script>
@@ -19,15 +16,24 @@
     },
     data () {
       return {
-        gutter: 0
+        gutter: 0,
       }
     },
-    created () {
-      console.log('col created')
-    },
-    mounted () {
-      console.log('col mounted')
-    },
+    computed: {
+      colClass () {
+        let {span, offset} = this
+        return [
+          span && `col-${span}`,
+          offset && `offset-${offset}`
+        ]
+      },
+      colStyle () {
+        return {
+          paddingLeft: this.gutter / 2 + 'px',
+          paddingRight: this.gutter / 2 + 'px',
+        }
+      }
+    }
   }
 </script>
 <style scoped lang="scss">
--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row" :style="{marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px'}">
+  <div class="row" :style="rowStyle">
     <slot></slot>
   </div>
 </template>
@@ -11,22 +11,18 @@
         type: [Number, String]
       }
     },
-    created () {
-      console.log('row created')
+    computed: {
+      rowStyle () {
+        let {gutter} = this
+        return {marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px'}
+      }
     },
     mounted () {
-      console.log('row mounted')
-      console.log(this.$children)
       this.$children.forEach((vm) => {
         vm.gutter = this.gutter
       })
     }
   }
-
-  var div = document.createElement('div') // created
-  var childDiv = document.createElement('div') // child created
-  div.appendChild(childDiv) // child mounted
-  document.body.appendChild(div) // mounted
 </script>
 <style scoped lang="scss">
   .row{
