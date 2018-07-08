61dc14f2babb4d3554e82698b2e98c000c1761fe
Frank Fang <FrankFang1990@gmail.com>
2018-07-08 14:31:35 +0800

    稍微重构了一下

--- src/col.vue
+++ src/col.vue
@@ -33,16 +33,25 @@
         gutter: 0,
       }
     },
+    methods: {
+      createClasses (obj, str = '') {
+        if (!obj) {return []}
+        let array = []
+        if (obj.span) { array.push(`col-${str}${obj.span}`) }
+        if (obj.offset) { array.push(`offset-${str}${obj.offset}`) }
+        return array
+      }
+    },
     computed: {
       colClass () {
         let {span, offset, ipad, narrowPc, pc, widePc} = this
+        let createClasses = this.createClasses
         return [
-          span && `col-${span}`,
-          offset && `offset-${offset}`,
-          ... (ipad ? [`col-ipad-${ipad.span}`] : []),
-          ... (narrowPc ? [`col-narrow-pc-${narrowPc.span}`] : []),
-          ... (pc ? [`col-pc-${pc.span}`] : []),
-          ... (widePc ? [`col-wide-pc-${widePc.span}`] : []),
+          ...createClasses({span, offset}),
+          ...createClasses(ipad, 'ipad-'),
+          ...createClasses(narrowPc, 'narrow-pc-'),
+          ...createClasses(pc, 'pc-'),
+          ...createClasses(widePc, 'wide-pc-'),
         ]
       },
       colStyle () {
