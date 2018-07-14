fe93b8edd4bfbb6390cbdf9882a0a3ed6e294edd
Frank Fang <FrankFang1990@gmail.com>
2018-07-14 22:21:38 +0800

    实现了只能有一个 toast 的功能

--- src/app.js
+++ src/app.js
@@ -40,21 +40,21 @@ new Vue({
     message: 'hi'
   },
   created(){
-    this.$toast('你的智商需要充值！', {
-      position: 'middle',
-      enableHtml: false,
-      closeButton: {
-        text: '已充值',
-        callback () {
-          console.log('他说已经充值智商了')
-        }
-      },
-      autoClose: false,
-      autoCloseDelay: 3
-    })
   },
   methods: {
     showToast(){
+      this.$toast(`你的智商目前为 ${parseInt(Math.random() * 100)}。你的智商需要充值！`, {
+        position: 'middle',
+        enableHtml: false,
+        closeButton: {
+          text: '已充值',
+          callback () {
+            console.log('他说已经充值智商了')
+          }
+        },
+        autoClose: false,
+        autoCloseDelay: 3
+      })
     }
   }
 })
--- src/plugin.js
+++ src/plugin.js
@@ -1,15 +1,48 @@
 import Toast from './toast'
 
+
+let currentToast
+
 export default {
   install (Vue, options) {
     Vue.prototype.$toast = function (message, toastOptions) {
-      let Constructor = Vue.extend(Toast)
-      let toast = new Constructor({
-        propsData: toastOptions
-      })
-      toast.$slots.default = [message]
-      toast.$mount()
-      document.body.appendChild(toast.$el)
+      if (currentToast) {
+        currentToast.close()
+      }
+      currentToast = createToast({Vue, message, propsData: toastOptions})
     }
   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* helpers */
+function createToast ({Vue, message, propsData}) {
+  let Constructor = Vue.extend(Toast)
+  let toast = new Constructor({propsData})
+  toast.$slots.default = [message]
+  toast.$mount()
+  document.body.appendChild(toast.$el)
+  return toast
 }
