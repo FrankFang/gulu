75d8e3578eb7cdf400d641a818bfd611e675fcd0
Frank Fang <FrankFang1990@gmail.com>
2018-07-21 20:42:36 +0800

    修复测试代码中 click 太快引发的 error

--- test/toast.test.js
+++ test/toast.test.js
@@ -26,7 +26,7 @@ describe('Toast', () => {
         done()
       })
     })
-    it('接受 closeButton', () => {
+    it('接受 closeButton', (done) => {
       const callback = sinon.fake();
       const Constructor = Vue.extend(Toast)
       const vm = new Constructor({
@@ -39,8 +39,11 @@ describe('Toast', () => {
       }).$mount()
       let closeButton = vm.$el.querySelector('.close')
       expect(closeButton.textContent.trim()).to.eq('关闭吧')
-      closeButton.click()
-      expect(callback).to.have.been.called
+      setTimeout(()=>{
+        closeButton.click()
+        expect(callback).to.have.been.called
+        done()
+      },200)
     })
     it('接受 enableHtml', () => {
       const Constructor = Vue.extend(Toast)
