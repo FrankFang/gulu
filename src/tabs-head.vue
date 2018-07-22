e1d92188d68904d55705f07a85e4df6e172f81f4
Frank Fang <FrankFang1990@gmail.com>
2018-07-22 14:42:49 +0800

    完善tabs的样式

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -23,11 +23,13 @@
 <style scoped lang="scss">
   $tab-height: 40px;
   $blue: blue;
+  $border-color: #ddd;
   .tabs-head {
     display: flex;
     height: $tab-height;
     justify-content: flex-start;
     position: relative;
+    border-bottom: 1px solid $border-color;
     > .line {
       position: absolute;
       bottom: 0;
@@ -36,6 +38,10 @@
     }
     > .actions-wrapper {
       margin-left: auto;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 0 1em;
     }
   }
 </style>
--- src/tabs-pane.vue
+++ src/tabs-pane.vue
@@ -34,8 +34,8 @@
 </script>
 <style lang="scss" scoped>
   .tabs-pane {
+    padding: 1em;
     &.active {
-      background: red;
     }
   }
 </style>
