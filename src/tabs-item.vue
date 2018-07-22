4b3b82f0c0e280b5ed0fb0365749624f1971b32f
Frank Fang <FrankFang1990@gmail.com>
2018-07-22 14:45:27 +0800

    支持 disabled 功能

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tabs-item" @click="xxx" :class="classes">
+  <div class="tabs-item" @click="onClick" :class="classes">
     <slot></slot>
   </div>
 </template>
@@ -25,7 +25,8 @@
     computed: {
       classes () {
         return {
-          active: this.active
+          active: this.active,
+          disabled: this.disabled
         }
       }
     },
@@ -35,7 +36,8 @@
       })
     },
     methods: {
-      xxx () {
+      onClick () {
+        if (this.disabled) { return }
         this.eventBus.$emit('update:selected', this.name, this)
       }
     }
@@ -43,6 +45,7 @@
 </script>
 <style lang="scss" scoped>
   $blue: blue;
+  $disabled-text-color: grey;
   .tabs-item {
     flex-shrink: 0;
     padding: 0 1em;
@@ -54,6 +57,9 @@
     &.active {
       color: $blue;
       font-weight: bold;
+    }
+    &.disabled {
+      color: $disabled-text-color;
     }
   }
 </style>
