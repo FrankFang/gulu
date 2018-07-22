e2687304be1abaa0e938c1d6b1f35c4e298c8fc2
Frank Fang <FrankFang1990@gmail.com>
2018-07-22 14:39:39 +0800

    tab 切换动画完成

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -11,9 +11,11 @@
   export default {
     name: 'GuluTabsHead',
     inject: ['eventBus'],
-    created () {
+    mounted () {
       this.eventBus.$on('update:selected', (item, vm) => {
-        console.log(item)
+        let {width, height, top, left} = vm.$el.getBoundingClientRect()
+        this.$refs.line.style.width = `${width}px`
+        this.$refs.line.style.left = `${left}px`
       })
     }
   }
@@ -25,13 +27,12 @@
     display: flex;
     height: $tab-height;
     justify-content: flex-start;
-    border: 1px solid red;
     position: relative;
     > .line {
       position: absolute;
       bottom: 0;
       border-bottom: 1px solid $blue;
-      width: 100px;
+      transition: all 350ms;
     }
     > .actions-wrapper {
       margin-left: auto;
