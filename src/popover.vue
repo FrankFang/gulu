752c1e4c633a1d1bc343a849f32259db1403f865
Frank Fang <FrankFang1990@gmail.com>
2018-07-22 16:18:34 +0800

    实现最简单的 popover

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="popover" @click="xxx">
-    <div class="content-wrapper" v-if="visible">
+  <div class="popover" @click.stop="xxx">
+    <div class="content-wrapper" v-if="visible" @click.stop>
       <slot name="content"></slot>
     </div>
     <slot></slot>
@@ -16,6 +16,19 @@
     methods: {
       xxx () {
         this.visible = !this.visible
+        if (this.visible === true) {
+          this.$nextTick(() => {
+            let eventHandler = () => {
+              this.visible = false
+              console.log('document 隐藏 popover')
+              document.removeEventListener('click', eventHandler)
+            }
+
+            document.addEventListener('click', eventHandler)
+          })
+        }else{
+          console.log('vm 隐藏 popover')
+        }
       }
     }
   }
