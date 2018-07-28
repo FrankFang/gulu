2c02a4eb0008a277534a25a96e1d9b16b35b175b
Frank Fang <FrankFang1990@gmail.com>
2018-07-28 20:31:55 +0800

    解决当容器有 overflow hidden 造成的 bug

--- src/app.js
+++ src/app.js
@@ -51,9 +51,8 @@ new Vue({
   created(){
   },
   methods: {
-    yyy(data){
+    yyy(){
       console.log('yyy')
-      console.log(data)
     },
     showToast1(){
       this.showToast('top')
--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="popover" @click.stop="xxx">
-    <div class="content-wrapper" v-if="visible" @click.stop>
+    <div ref="contentWrapper" class="content-wrapper"
+      v-if="visible">
       <slot name="content"></slot>
     </div>
-    <slot></slot>
+    <span ref="triggerWrapper">
+      <slot></slot>
+    </span>
   </div>
 </template>
 
@@ -18,9 +21,12 @@
         this.visible = !this.visible
         if (this.visible === true) {
           this.$nextTick(() => {
+            document.body.appendChild(this.$refs.contentWrapper)
+            let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+            this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+            this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
             let eventHandler = () => {
               this.visible = false
-              console.log('document 隐藏 popover')
               document.removeEventListener('click', eventHandler)
             }
 
@@ -39,12 +45,11 @@
     display: inline-block;
     vertical-align: top;
     position: relative;
-    .content-wrapper {
-      position: absolute;
-      bottom: 100%;
-      left: 0;
-      border: 1px solid red;
-      box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-    }
+  }
+  .content-wrapper {
+    position: absolute;
+    border: 1px solid red;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    transform: translateY(-100%);
   }
 </style>
