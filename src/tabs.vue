167decdada004306b9177e54a728a373f7ec75d4
Frank Fang <FrankFang1990@gmail.com>
2018-07-28 21:24:52 +0800

    重构 tabs.vue

--- src/tabs.vue
+++ src/tabs.vue
@@ -30,21 +30,29 @@
         eventBus: this.eventBus
       }
     },
-    mounted () {
-      if (this.$children.length === 0) {
-        console && console.warn &&
-        console.warn('tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件')
-      }
-      this.$children.forEach((vm) => {
-        if (vm.$options.name === 'GuluTabsHead') {
-          vm.$children.forEach((childVm) => {
-            if (childVm.$options.name === 'GuluTabsItem'
-              && childVm.name === this.selected) {
-              this.eventBus.$emit('update:selected', this.selected, childVm)
-            }
-          })
+    methods: {
+      checkChildren () {
+        if (this.$children.length === 0) {
+          console && console.warn &&
+          console.warn('tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件')
         }
-      })
+      },
+      selectTab () {
+        this.$children.forEach((vm) => {
+          if (vm.$options.name === 'GuluTabsHead') {
+            vm.$children.forEach((childVm) => {
+              if (childVm.$options.name === 'GuluTabsItem'
+                && childVm.name === this.selected) {
+                this.eventBus.$emit('update:selected', this.selected, childVm)
+              }
+            })
+          }
+        })
+      }
+    },
+    mounted () {
+      this.checkChildren();
+      this.selectTab();
     }
   }
 </script>
