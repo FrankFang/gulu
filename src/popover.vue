7eb014783d2e274096cb0a5ad3ca0263806aa7ea
Frank Fang <FrankFang1990@gmail.com>
2018-07-28 22:33:47 +0800

    支持两种事件触发

--- src/popover.vue
+++ src/popover.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="popover" @click="onClick" ref="popover">
+  <div class="popover" ref="popover">
     <div ref="contentWrapper" class="content-wrapper" v-if="visible"
       :class="{[`position-${position}`]:true}">
       <slot name="content"></slot>
@@ -14,7 +14,41 @@
   export default {
     name: "GuluPopover",
     data () {
-      return {visible: false}
+      return {
+        visible: false,
+      }
+    },
+    mounted () {
+      if (this.trigger === 'click') {
+        this.$refs.popover.addEventListener('click', this.onClick)
+      } else {
+        this.$refs.popover.addEventListener('mouseenter', this.open)
+        this.$refs.popover.addEventListener('mouseleave', this.close)
+      }
+    },
+    destroyed () {
+      if (this.trigger === 'click') {
+        this.$refs.popover.removeEventListener('click', this.onClick)
+      } else {
+        this.$refs.popover.removeEventListener('mouseenter', this.open)
+        this.$refs.popover.removeEventListener('mouseleave', this.close)
+      }
+    },
+    computed: {
+      openEvent () {
+        if (this.trigger === 'click') {
+          return 'click'
+        } else {
+          return 'mouseenter'
+        }
+      },
+      closeEvent () {
+        if (this.trigger === 'click') {
+          return 'click'
+        } else {
+          return 'mouseleave'
+        }
+      }
     },
     props: {
       position: {
@@ -22,6 +56,13 @@
         default: 'top',
         validator (value) {
           return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
+        }
+      },
+      trigger: {
+        type: String,
+        default: 'click',
+        validator (value) {
+          return ['click', 'hover'].indexOf(value) >= 0
         }
       }
     },
@@ -76,7 +117,6 @@
             this.open()
           }
         }
-
       }
     }
   }
