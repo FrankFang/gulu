00df9df92b69c14ec5c65ed8b6e8ae724d5e4160
Frank Fang <FrankFang1990@gmail.com>
2018-08-04 20:28:06 +0800

    更新 demo

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -3,7 +3,7 @@
     <div class="title" @click="toggle">
       {{title}}
     </div>
-    <div class="content" v-if="open">
+    <div class="content" ref="content" v-if="open">
       <slot></slot>
     </div>
   </div>
@@ -55,6 +55,7 @@
   .collapseItem {
     > .title { border: 1px solid $grey; margin-top: -1px; margin-left: -1px; margin-right: -1px;
       min-height: 32px; display: flex; align-items: center; padding: 0 8px;
+      background: lighten($grey, 8%);
     }
     &:first-child {
       > .title { border-top-left-radius: $border-radius; border-top-right-radius: $border-radius; }
@@ -63,5 +64,6 @@
       > .title:last-child { border-bottom-left-radius: $border-radius; border-bottom-right-radius: $border-radius; }
     }
     > .content { padding: 8px; }
+
   }
 </style>
