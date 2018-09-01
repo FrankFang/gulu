1561ebe027a6a61faa67752e05a020c8eedef519
Frank Fang <FrankFang1990@gmail.com>
2018-09-01 20:23:44 +0800

    把 destroyed 改为 beforeDestroy

--- src/popover.vue
+++ src/popover.vue
@@ -42,7 +42,7 @@
         this.$refs.popover.addEventListener('mouseleave', this.close)
       }
     },
-    destroyed () {
+    beforeDestroy () {
       if (this.trigger === 'click') {
         this.$refs.popover.removeEventListener('click', this.onClick)
       } else {
